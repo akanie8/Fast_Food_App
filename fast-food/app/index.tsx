e4fbf853f2876b6767d9b434403996069d9cb85b
akanie8 <docsean2001@gmail.com>
2025-08-05 13:05:40 +0200

    new fonts and initializing color contracts

--- fast-food/app/index.tsx
+++ fast-food/app/index.tsx
@@ -1,11 +1,36 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-   <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-orange-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList 
+      data={offers} 
+      renderItem={({item, index}) => {
+          const isEven = index % 2 === 0;
+        return (
+          <Pressable className='offer-card' style={{backgroundColor: item.color}}>
+              {({pressed}) =>(
+                <Fragment>
+                  <View className={"h-full w-1/2"}>
+                      <Image
+                      source={item.image}
+                      className={"size-full"}
+                      resizeMode={"contain"}
+                      />
+                  </View>
+                  <View className={"offer-card_info"}>
+                      {item.title}
+                  </View>
+                </Fragment>
+              )}
+          </Pressable>
+        )
+      }}
+      />
+    </SafeAreaView>
   );
 }
+ 
